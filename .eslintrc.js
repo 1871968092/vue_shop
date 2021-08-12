c06249e3d7a7381e25745d29a64cbefd936aedc7
1871968092 <1871968092@qq.com>
2021-08-12 21:06:32 +0800

    完成了分类参数的开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'space-before-function-paren': 0,
+    'camelcase': 'off',
+    '@typescript-eslint/camelcase': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'indent': 'off'
