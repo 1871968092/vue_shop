90548e93d4abf7faa2cc2e52677db76025fb2e77
1871968092 <1871968092@qq.com>
2021-08-15 16:12:14 +0800

    完成商品功能开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,39 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
     'space-before-function-paren': 0,
-    'camelcase': 'off',
+    camelcase: 'off',
     '@typescript-eslint/camelcase': 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'indent': 'off'
-  }
+    'no-console': 'off',
+    'no-debugger': 'off',
+    indent: 'off',
+    'vue/no-async-in-computed-properties': 'off',
+    'vue/no-dupe-keys': 'off',
+    'vue/no-duplicate-attributes': 'off',
+    'vue/no-parsing-error': 'off',
+    'vue/no-reserved-keys': 'off',
+    'vue/no-shared-component-data': 'off',
+    'vue/no-side-effects-in-computed-properties': 'off',
+    'vue/no-template-key': 'off',
+    'vue/no-textarea-mustache': 'off'
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ]
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -10,7 +10,9 @@ import {
     Tooltip, Pagination, Dialog,
     MessageBox, Tag, Tree,
     Select, Option, Cascader,
-    Alert, Tabs, TabPane
+    Alert, Tabs, TabPane,
+    Steps, Step, Checkbox,
+    CheckboxGroup, Upload
 } from 'element-ui'
 
 Vue.use(Button)
@@ -44,5 +46,10 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
